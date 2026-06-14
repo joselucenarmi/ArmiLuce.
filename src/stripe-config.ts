3376ffe85b92ce187792eadd7615dfa016194af0
joselucenarmi <joselucenarmi37@gmail.com>
2026-06-14 16:24:49 +0200

    Updated stripe-config.ts

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -10,10 +10,28 @@ export interface StripeProduct {
 
 export const STRIPE_PRODUCTS: StripeProduct[] = [
   {
-    priceId: 'price_1Thw9GRSWCND59VhMYrl5Hmb',
-    name: 'Producto de prueba',
+    priceId: 'price_1ThxAcIXfL8TnkwC2idYT1DL',
+    name: 'Plan 1 mes',
     description: 'Suscripción mensual para acceso completo a todas las funcionalidades premium',
-    price: 19.99,
+    price: 2.49,
+    currency: 'eur',
+    currencySymbol: '€',
+    mode: 'subscription'
+  },
+  {
+    priceId: 'price_1Ti3t4IXfL8TnkwCiz2juMzI',
+    name: 'Plan 3 meses',
+    description: 'Suscripción por 3 meses para acceso completo a todas las funcionalidades premium',
+    price: 6.49,
+    currency: 'eur',
+    currencySymbol: '€',
+    mode: 'subscription'
+  },
+  {
+    priceId: 'price_1Ti3uWIXfL8TnkwCaAAe36qE',
+    name: 'Plan 6 meses',
+    description: 'Suscripción por 6 meses para acceso completo a todas las funcionalidades premium',
+    price: 11.49,
     currency: 'eur',
     currencySymbol: '€',
     mode: 'subscription'
